2ba879d147847562ae5c066fda1f606c4de1267d
Pablo Ferrari <pferrari@gmail.com>
2024-01-18 22:35:49 -0300

    Created views to retrieve client / user settings with all information
    Releated to #1

--- postgresql/05.views.sql
+++ postgresql/05.views.sql
@@ -14,3 +14,13 @@ create view view_gen_data as select cli_id, gen_id, date_trunc('minute', data_da
 create view view_sta_data as select cli_id, sta_id, date_trunc('minute', data_date + interval '00:00:00.500') as data_date, data_type_id, data_pro_id, data_value, data_date_added from sta_data;--
 
 create view view_documents_sorted as select *, COALESCE(doc_date_from, doc_date_added) as doc_date_sort from document;--
+
+CREATE VIEW vw_cli_settings AS
+select * from 
+settings s left join cli_setting cs on s.set_name = cs.cli_set_name
+where s.set_flags ilike '_1%';--
+
+CREATE VIEW vw_usr_settings AS
+select * from 
+settings s left join usr_setting us on s.set_name = us.usr_set_name
+where s.set_flags ilike '1%';--
