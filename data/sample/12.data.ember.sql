c32b4f4ed6b55889b9f396d08293fbb28f125c39
Pablo Ferrari <pferrari@gmail.com>
2024-05-07 22:35:51 -0300

    Removed ember data import. Solarec-java / solarec.inverters has a service for it now.

--- data/sample/12.data.ember.sql
+++
@@ -1,4 +0,0 @@
-delete from ember_country_overview;
-
--- Source: https://raw.githubusercontent.com/ember-climate/ember-data-api/main/data/api_country_overview_yearly.csv
-COPY ember_country_overview FROM '/path/to/your_data.csv' WITH CSV HEADER;--
